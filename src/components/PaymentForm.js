64c014adbe538e98523742c1958ee4b034b2863f
unknown <interpercelservice+2@gmail.com>
2022-01-10 18:43:48 +0100

    little bug fixed

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { PageHero } from '.';
 import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
+
 
  const PaymentForm = () => {
    const config = {
-    public_key: 'FLWPUBK-**************************-X',
+    public_key: 'FLWPUBK-796a103deb59564fb2210fa637ed13bf-A',
     tx_ref: Date.now(),
     amount: 100,
     currency: 'NGN',
     payment_options: 'card,mobilemoney,ussd',
     customer: {
-      email: 'user@gmail.com',
-      phonenumber: '07064586146',
-      name: 'joel ugwumadu',
+      email: 'interpercelservice@gmail.com',
+      phonenumber: '08064180322',
+      name: 'Ayodeji Adebayo',
     },
     customizations: {
-      title: 'My store',
+      title: 'Lummy-store',
       description: 'Payment for items in cart',
       logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
     },
@@ -26,17 +26,20 @@ import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
     text: 'Pay with Flutterwave!',
     callback: (response) => {
        console.log(response);
-      closePaymentModal() // this will close the modal programmatically
+      closePaymentModal() // this will close the modal programmatically 
+      // X
     },
     onClose: () => {},
   };
 
   return (
     <div className="App">
-     <h1>Hello Test user</h1>
-      <FlutterWaveButton {...fwConfig} />
+     <h1>Make payment here</h1>
+      <FlutterWaveButton {...fwConfig} className='btn' />
     </div>
   );
-}
+
+
+  };
 export default PaymentForm
 
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -8,10 +8,33 @@ import { Link } from 'react-router-dom'
 const CheckoutPage = () => {
   return <main>
     <PageHero title="payment"/>
-    <Wrapper className='page'></Wrapper>
-    <h1>check out here</h1>
-    <PaymentForm/>
+    <Wrapper className='page'>
+       <PaymentForm/>
+    </Wrapper>
   </main>
 }
-const Wrapper = styled.div``     
+const Wrapper = styled.div`
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+ 
+  text-align: center;
+  span {
+    color: var(--clr-primary-5);
+  }
+  h5 {
+    color: var(--clr-white);
+    margin: 0.1rem;
+
+    font-weight: 400;
+    text-transform: none;
+    line-height: 1.25;
+  }
+  @media (min-width: 776px) {
+    flex-direction: row;
+  }
+
+`     
 export default CheckoutPage
